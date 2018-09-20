a8e631046eda8c4554bd3a451e64cb75fe95264d
Sam Levenick <slevenick@google.com>
2018-09-20 20:29:01 +0000

    return_if_object raises error if a 404 is received on a create update or delete request

--- lib/puppet/provider/gresourcemanager_project/google.rb
+++ lib/puppet/provider/gresourcemanager_project/google.rb
@@ -188,10 +188,10 @@ Puppet::Type.type(:gresourcemanager_project).provide(:google) do
   end
 
   # rubocop:disable Metrics/CyclomaticComplexity
-  def self.return_if_object(response)
+  def self.return_if_object(response, allow_not_found = false)
     raise "Bad response: #{response}" \
       unless response.is_a?(Net::HTTPResponse)
-    return if response.is_a?(Net::HTTPNotFound)
+    return if response.is_a?(Net::HTTPNotFound) && allow_not_found
     return if response.is_a?(Net::HTTPNoContent)
     # TODO(nelsonjr): Remove return of Net::HTTPForbidden from
     # return_if_object once Cloud SQL bug http://b/62635365 is resolved.
@@ -199,7 +199,7 @@ Puppet::Type.type(:gresourcemanager_project).provide(:google) do
     # when the user has access to the project. This is being changed to
     # return 404 as it is supposed to be.  Once 404 is the correct
     # response the temporary workaround should be removed.
-    return if response.is_a?(Net::HTTPForbidden)
+    return if response.is_a?(Net::HTTPForbidden) && allow_not_found
     result = JSON.parse(response.body)
     raise_if_errors result, %w[error errors], 'message'
     raise "Bad response: #{response}" unless response.is_a?(Net::HTTPOK)
@@ -207,8 +207,8 @@ Puppet::Type.type(:gresourcemanager_project).provide(:google) do
   end
   # rubocop:enable Metrics/CyclomaticComplexity
 
-  def return_if_object(response)
-    self.class.return_if_object(response)
+  def return_if_object(response, allow_not_found = false)
+    self.class.return_if_object(response, allow_not_found)
   end
 
   def self.extract_variables(template)
@@ -235,7 +235,7 @@ Puppet::Type.type(:gresourcemanager_project).provide(:google) do
     get_request = ::Google::Resourcemanager::Network::Get.new(
       self_link, fetch_auth(resource)
     )
-    return_if_object get_request.send
+    return_if_object get_request.send, true
   end
 
   def self.raise_if_errors(response, err_path, msg_field)
